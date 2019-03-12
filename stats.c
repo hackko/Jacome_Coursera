ecd9d6f81c9b080d2d206fbdebed03f49f0bfba6
hackko <j.kome@hotmail.com>
2019-03-12 10:47:06 -0700

    final dev_c1m1

--- stats.c
+++ stats.c
@@ -23,6 +23,7 @@
 
 #include <stdio.h>
 #include "stats.h"
+#include <stdint.h>
 
 /* Size of the Data Set */
 #define SIZE (40)
@@ -37,9 +38,110 @@ void main() {
 
   /* Other Variable Declarations Go Here */
   /* Statistics and Printing Functions Go Here */
+	printf ("test array : \n");	
+	print_array (test, SIZE);
+	print_statistics (test, SIZE);
+
+
 
 }
 
 /* Add other Implementation File Code Here */
 
+void print_statistics (unsigned char *array, unsigned int length){
 
+	printf("sorted array: \n");
+	sort_array(array, length);
+	printf ("stadisticas relacionadas con el test son las siguientes: \n");
+	printf ("median = %d \r\n", find_median(array, length));
+	printf ("mean = %d \r\n" , find_mean(array,length));
+	printf("maximum = %d\r\n", find_maximum(array,length));
+	printf("minimum = %d \r\n ", find_minimum (array, length));
+
+
+}
+
+void print_array(unsigned char *array, unsigned int length ){
+	int i;
+	for (i = 0; i < length; i++){
+		printf("array[%d] = %d \r\n ", i , (int)array[i]);
+
+	}
+}
+
+int find_median (unsigned char *array, unsigned int length ){
+	unsigned int i,j;
+	float temp;
+	for (i=0; i<length; i++){
+		for (j=i+1; j < length; j++){
+			if (array[i] > array[j]){
+				temp = (int)array[j];
+				array[j]=(int)array[i];
+				array[i] = temp;
+			}
+		}
+	}
+
+	if (length % 2 == 0){
+		return ((int)array[length/2] + (int)array[length/2 -1]) /2;
+
+	}else{
+		return (int)array[length/2];
+	}
+
+}
+
+
+int find_mean (unsigned char *array, unsigned int length ){
+		int sum = 0 , i;
+		for (i=0;i<length;i++){
+			sum+=(int)array[i];
+		}
+		return ((float)sum/length);
+}
+
+int find_maximum (unsigned char *array, unsigned int length ){
+	int max, index;
+
+	/*Asumimos el primer elemento del array como maximo */
+       max = (int)array[0];
+	index = 0;
+
+	for (index = 1; index < length ; ++index){
+		if (array[index] > max){
+			max = (int)array[index];
+		}
+	}
+	return max;
+
+}
+
+int find_minimum (unsigned char *array, unsigned int length ){
+	int min, index;
+	/* Asumimos que es el primer elemento del array es el minimo */
+	min = (int)array[0];
+	index = 0;
+
+	for (index = 1; index < length; ++index){
+		if (array[index] < min){
+			min = (int)array[index];
+		}
+	}
+	return min;
+}
+
+void sort_array (unsigned char *array, unsigned int length){
+	int i,j;
+	int temp;
+
+	for (i=0; i< length; i++){
+		for (j= i+1;j <length; j++){
+			if (array[i] < array[j]){
+				temp = (int)array[j];
+				array[j] = (int)array[i];
+				array[i] = temp;
+			}
+		}
+		printf ("array[%d] = %d\r\n ",i, (int)array[i]);	
+	}
+}
